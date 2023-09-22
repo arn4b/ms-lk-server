73f10e124fe142dfe49373e63e01a99104d1c836
Arnab Chatterjee <arnabc1701@gmail.com>
2023-09-22 12:02:28 +0530

    roomId added

--- server.js
+++ server.js
@@ -3,12 +3,32 @@ const cors = require('cors');
 
 require('dotenv').config();
 
-const { AccessToken } = require('livekit-server-sdk');
+const { AccessToken, RoomServiceClient } = require('livekit-server-sdk');
 
-const createToken = ({ canPublish, userName }) => {
+const createRoom = async (roomId) => {
+    const livekitHost = 'https://spacev2-demo-17wvllxz.livekit.cloud';
+    const roomService = new RoomServiceClient(livekitHost, process.env.LK_API_KEY, process.env.LK_API_SECRET);
+
+    const opts = {
+        name: roomId,
+        emptyTimeout: 10 * 60, // 10 minutes
+        maxParticipants: 20,
+    };
+
+    try {
+        const room = await roomService.createRoom(opts);
+        console.log('room created', room);
+        return room.name;
+    } catch (error) {
+        console.error('Error creating room:', error);
+        throw error;
+    }
+}
+
+const createToken = async ({ canPublish, userName, roomId }) => {
     // if this room doesn't exist, it'll be automatically created when the first
     // client joins
-    const roomName = 'spacev2-demo';
+    const roomName = await createRoom(roomId);
     // identifier to be used for participant.
     // it's available as LocalParticipant.identity with livekit-client SDK
     const participantName = userName;
@@ -16,9 +36,6 @@ const createToken = ({ canPublish, userName }) => {
     const at = new AccessToken(process.env.LK_API_KEY, process.env.LK_API_SECRET, {
         identity: participantName,
     });
-
-    console.log(userName)
-    console.log(canPublish)
 
     at.addGrant({
         roomJoin: true,
@@ -35,9 +52,10 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.get('/token', (req, res) => {
+app.get('/token', async (req, res) => {
     const userType = req.query.userType;
     const userName = req.query.userName;
+    const roomId = req.query.roomId;
 
     let canPublish = false;
     if (userType === 'sender') {
@@ -46,10 +64,18 @@ app.get('/token', (req, res) => {
         canPublish = false;
     }
 
-    const token = createToken({ canPublish, userName });
+    const token = await createToken({ canPublish, userName, roomId });
+    console.log("🚀 ~ file: server.js:68 ~ app.get ~ token:", token)
 
     res.send(token);
 });
+
+app.get('/createRoom', (req, res) => {
+    const roomId = req.query.roomId;
+
+    const room = createRoom(roomId)
+    res.send(room);
+})
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
